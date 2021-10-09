77c31457e612fc21b582f09b1c50a5eb4dec4a5e
bodhert <alejandro.cordoba.bodhert@gmail.com>
2021-10-09 01:23:37 +0000

    wip

---
+++ discuss/web/controllers/topic_controller.ex
@@ -0,0 +1,15 @@
+defmodule Discuss.TopicController do
+  use Discuss.Web, :controller
+
+  alias Discuss.Topic
+
+  def new(conn, params) do
+    changeset = Topic.changeset(%Topic{}, %{})
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"topic" => topic} = params) do
+    IO.inspect(params, label: "----params----")
+  end
+
+end
