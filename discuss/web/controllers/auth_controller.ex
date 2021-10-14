893db3cb6e4c43470eba45960cd9fe8f9d5e35bc
bodhert <alejandro.cordoba.bodhert@gmail.com>
2021-10-14 16:08:13 +0000

    wip

--- discuss/web/controllers/auth_controller.ex
+++ discuss/web/controllers/auth_controller.ex
@@ -2,8 +2,11 @@ defmodule Discuss.AuthController do
   use Discuss.Web, :controller
   plug Ueberauth
 
-  def callback(conn,params) do
-    IO.inspect(conn.assigns, label: "+++conn++")
-    IO.inspect(params, label: "+++params++")
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
+    %{"provider" => provider} = params
+    user_params = %{token: auth.credentials.token, email: auth.info.email, provider: provider}
+    IO.inspect(user_params, label: "user_params")
+    IO.inspect(auth, label: "+++auth++")
+    IO.inspect(provider, label: "+++params++")
   end
 end
