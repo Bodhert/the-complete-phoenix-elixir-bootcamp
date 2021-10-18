de1fa9d8be3ade245745edf55870d736ff161de4
bodhert <alejandro.cordoba.bodhert@gmail.com>
2021-10-18 17:11:23 +0000

    wip

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -3,8 +3,9 @@ defmodule Discuss.TopicController do
 
   alias Discuss.Topic
 
+  plug Discuss.Plugs.RequireAuth when action in [:new, :create, :edit, :update, :delete]
+
   def index(conn, _params) do
-    IO.inspect(conn.assigns, label: "---conn---")
     topics = Repo.all(Topic)
     render conn, "index.html", topics: topics
   end
