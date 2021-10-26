be7bdd1201cb05ba2ff28d7cf32f373c2daf9f83
bodhert <alejandro.cordoba.bodhert@gmail.com>
2021-10-26 15:44:16 +0000

    wip

--- discuss/web/channels/comments_channel.ex
+++ discuss/web/channels/comments_channel.ex
@@ -1,10 +1,29 @@
 defmodule Discuss.CommentsChannel do
   use Discuss.Web, :channel
-  def join(name, _params, socket) do
-    IO.inspect(name, label: "___name____")
-    {:ok, %{hey: "there"}, socket}
+  alias Discuss.{Topic, Comment}
+
+  def join("comments:" <> topic_id, _params, socket) do
+    topic_id = String.to_integer(topic_id)
+    topic = Topic
+      |> Repo.get(topic_id)
+      |> Repo.preload(:comments)
+
+    {:ok, %{comments: topic.comments}, assign(socket, :topic, topic)}
   end
 
-  def handle_in() do
+  def handle_in(name, %{"content" => content}, socket) do
+    topic = socket.assigns.topic
+    changeset = topic
+    |> build_assoc(:comments)
+    |> IO.inspect(label: "build_assoc")
+    |> Comment.changeset(%{content: content})
+
+    case Repo.insert(changeset) do
+      {:ok, _comment} ->
+        {:reply, :ok, socket}
+      {:error, _reason} ->
+        {:reply, {:error, %{errors: changeset}}, socket}
+    end
+
   end
 end
